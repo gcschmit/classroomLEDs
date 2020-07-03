8a91328b3ca05bde26f656996ebab138489965ab
Geoff Schmit <gcschmit@gmail.com>
2020-07-03 16:53:55 -0500

    read the LED mode from the server

--- raspi/classroomLEDs.py
+++ raspi/classroomLEDs.py
@@ -6,19 +6,21 @@ import adafruit_dotstar as dotstar
 import board
 
 num_pixels = 10
+led_modes = {"solid": 0, "pulse": 1}
 
 # Use SPI on the Raspberry Pi which is faster than bit banging.
 # Explicitly slow the baud rate to 16 MHz using the undocumented parameter which
-#	appears to improve reliability.
+#   appears to improve reliability.
 # The pixel order of the DotStar strips that we have appear to be BGR, but there are
-#	still unresolved issues regarding colors.	
+#   still unresolved issues regarding colors.   
 pixels= dotstar.DotStar(board.SCK, board.MOSI, num_pixels, brightness=0.2, pixel_order=dotstar.BGR, auto_write=False, baudrate=16000000)
 
 while True:
     led_color = (0, 0, 0)
+    led_mode = 0 # 0: solid; 1: pulse
 
     try:
-    	# eventually, update the URL to that for the server running on EC2
+        # eventually, update the URL to that for the server running on EC2
         url = "http://192.168.1.139:3000/leds/1"
         response = requests.get(url = url)
         response.raise_for_status()
@@ -36,7 +38,7 @@ while True:
     print(scenes)
     
     # don't assume that the scenes are sorted by time; it is important that they are
-    #	since the LEDs will be set to the most recent scence whose time has passed
+    #   since the LEDs will be set to the most recent scence whose time has passed
     scenes.sort(key=lambda k: k['time'])
     
     for scene in scenes:
@@ -54,15 +56,18 @@ while True:
         print(now)
         
         if sch_time < now:
-        	# the color can be specified as a tuple with 4 elements: (R, G, B, brightness)
+            # the color can be specified as a tuple with 4 elements: (R, G, B, brightness)
             led_color = tuple(int(scene["color"][i:i+2], 16) for i in (2, 4, 6))
             led_color = led_color + (scene["brightness"],)
             print(led_color)
-        
+            led_mode = led_modes{scene["mode"]}
+            print(led_mode)
+    
     pixels.fill(led_color)
+    
     pixels.show()
     
     # a more sophisticated approach is needed where the server is checked only 
-    #	occasionally but the LEDs are updated based on the last-read schedule more
-    #	frequently
+    #   occasionally but the LEDs are updated based on the last-read schedule more
+    #   frequently
     time.sleep(10)

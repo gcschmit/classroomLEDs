1797ad0bf358a2cc0cb4c0601311f2ad27dd658d
Geoff Schmit <gcschmit@gmail.com>
2020-08-13 14:37:08 -0500

    removed unused method and imports

--- mobileApp/classroom_leds/lib/util/navigation_util.dart
+++ mobileApp/classroom_leds/lib/util/navigation_util.dart
@@ -1,16 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:classroom_leds/model/scene.dart';
 import 'package:classroom_leds/page/edit_scene_page.dart';
-//import 'package:classroom_leds/page/view_scene_page.dart';
 
 Future navigateToEditScenePage(Scene scene, BuildContext context) {
   return Navigator.of(context).push(
     MaterialPageRoute(builder: (context) => EditScenePage(scene)),
   );
-}
-
-Future navigateToAddScenePage(BuildContext context) {
-  /*return Navigator.of(context).push(
-    MaterialPageRoute(builder: (context) => AddScenePage()),
-  );*/
 }
--- mobileApp/classroom_leds/lib/util/server_util.dart
+++ mobileApp/classroom_leds/lib/util/server_util.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:convert';
 
-import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:classroom_leds/model/scene.dart';
 

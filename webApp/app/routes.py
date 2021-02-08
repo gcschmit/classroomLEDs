b400d606c13aa30ed639df84cc4ed016fa28b441
BZheng2963 <xml2963x@gmail.com>
2021-02-08 14:54:48 -0600

    Finished Templates for WebApp

--- webApp/app/routes.py
+++ webApp/app/routes.py
@@ -1,7 +1,19 @@
+from flask import render_template
 from app import app #importing the app variable (right) defined in the app package (left)
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello, World!'
+    user = {'username': 'Bill'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Paris!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'Classroom LEDs work!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)

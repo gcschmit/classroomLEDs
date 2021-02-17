d888613b71e47137d6eeb9b21b369a45a963831f
BZheng2963 <xml2963x@gmail.com>
2021-02-17 10:48:29 -0600

    Finished Database for WebApp

---
+++ webApp/app/models.py
@@ -0,0 +1,22 @@
+# Writing a class for each type of entity we will be storing in the database
+from datetime import datetime
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+class Post(db.Model): # This is probably where we play around with schedules!
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow) #UTC time zone
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)

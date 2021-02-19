b2ffa9eb73e7a541bd8c87ab5eabd59747f0d66e
BZheng2963 <xml2963x@gmail.com>
2021-02-18 22:21:14 -0600

    Finished User Logins for WebApp

--- webApp/app/models.py
+++ webApp/app/models.py
@@ -1,8 +1,10 @@
 # Writing a class for each type of entity we will be storing in the database
 from datetime import datetime
-from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import db, login
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -11,6 +13,16 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 class Post(db.Model): # This is probably where we play around with schedules!
     id = db.Column(db.Integer, primary_key=True)
--- webApp/app/routes.py
+++ webApp/app/routes.py
@@ -1,11 +1,14 @@
-from flask import render_template, flash, redirect, url_for
-from app import app #importing the app variable (right) defined in the app package (left)
-from app.forms import LoginForm
+from flask import render_template, flash, redirect, url_for, request
+from werkzeug.urls import url_parse
+from flask_login import current_user, login_user, logout_user, login_required
+from app import app, db #importing the app variable (right) defined in the app package (left)
+from app.forms import LoginForm, RegistrationForm
+from app.models import User
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'username': 'Bill'}
     posts = [
         {
             'author': {'username': 'John'},
@@ -16,13 +19,40 @@ def index():
             'body': 'Classroom LEDs work!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login(): #Figure out which 
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
+        user = User.query.filter_by(username=form.username.data).first() #finding user if stored in database
+        if user is None or not user.check_password(form.password.data): #invalid login attempts
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or not next_page.startswith('/'): #protect from malicious urls
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
